fdea95ac7fd46b1541524a63e5aebb8f9c291304
msunkist99 <msunkist99@gmail.com>
2019-01-26 18:51:21 -0600

    01/26/2019 for loop to print 1000 times

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -7,7 +7,10 @@ namespace Turtles
         public static void Main()
         {
             // TODO: Prints "I like turtles." 1000 times.
-
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine((i + 1) + " I like turtles.");
+            }
             Console.ReadLine();
         }
     }
