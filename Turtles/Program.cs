c1ff1977bb560c41a2a8e1cfe7f2f03a09f53218
msunkist99 <msunkist99@gmail.com>
2019-02-05 18:35:13 -0600

    2019/02/05 unit test case correction

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -9,7 +9,8 @@ namespace Turtles
             // TODO: Prints "I like turtles." 1000 times.
             for (int i = 0; i < 1000; i++)
             {
-                Console.WriteLine((i + 1) + " I like turtles.");
+               // Console.WriteLine((i + 1) + " I like turtles.");
+                Console.WriteLine("I like turtles.");
             }
             Console.ReadLine();
         }
